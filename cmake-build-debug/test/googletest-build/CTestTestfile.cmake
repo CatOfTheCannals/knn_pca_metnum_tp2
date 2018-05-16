7c75a6512f7eaf3aad380a15ff90ceb51af6ac8b
juan junqueras <jjunqueras@dc.uba.ar>
2018-05-16 17:52:24 -0300

    tests for function distance of kNN

---
+++ cmake-build-debug/test/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/juan/Documentos/CienciasDeLaComputacion/MetodosNumericos/TP2/entregaVersionCharly2/knn_pca_metnum_tp2/cmake-build-debug/test/googletest-src
+# Build directory: /home/juan/Documentos/CienciasDeLaComputacion/MetodosNumericos/TP2/entregaVersionCharly2/knn_pca_metnum_tp2/cmake-build-debug/test/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest")
--- test/kNN_test.cpp
+++ test/kNN_test.cpp
@@ -3,39 +3,130 @@
 #include <stdlib.h>
 #include "gtest/gtest.h"
 
+#include "../src/Matrix.hpp"
+#include "../src/kNN.hpp"
+
 // --------- SET UP --------------
-class kNNTest : public ::testing::Test {
+class runTest : public ::testing::Test {
 protected:
     virtual void SetUp() {
-        int i = 0;
-        while (i < 10){
-            int j = 0;
-            while (j < 10) {
-                bigArray[i][j] = rand() % 100;//Array con elementos random
-                j++;
-            }
-            i++;
-        }
-        i = 0;
-        while (i < 1000){
-            reallyBig[i] = rand() % 200;
-            i++;
-        }
+        f.setIndex(0, 0, 1);
+        f.setIndex(0, 1, 1);
+        f.setIndex(0, 2, 1);
+        f.setIndex(0, 3, 1);
+        f.setIndex(0, 0, 1);
+        f.setIndex(0, 1, 1);
+        f.setIndex(0, 2, 1);
+        f.setIndex(0, 3, 1);
+        f.setIndex(0, 0, 1);
+        f.setIndex(0, 1, 1);
+        f.setIndex(0, 2, 1);
+        f.setIndex(0, 3, 1);
+        f.setIndex(0, 0, 1);
+        f.setIndex(0, 1, 1);
+        f.setIndex(0, 2, 1);
+        f.setIndex(0, 3, 1);
+
+        g.setIndex(0, 0, 1);
+        g.setIndex(0, 1, 2);
+        g.setIndex(0, 2, 3);
+        g.setIndex(0, 3, 4);
+        g.setIndex(0, 0, 5);
+        g.setIndex(0, 1, 6);
+        g.setIndex(0, 2, 7);
+        g.setIndex(0, 3, 8);
+        g.setIndex(0, 0, 9);
+        g.setIndex(0, 1, 10);
+        g.setIndex(0, 2, 11);
+        g.setIndex(0, 3, 12);
+        g.setIndex(0, 0, 13);
+        g.setIndex(0, 1, 14);
+        g.setIndex(0, 2, 15);
+        g.setIndex(0, 3, 16);
+
+        x_0.setIndex(0, 0, 1);
+        x_0.setIndex(1, 0, 1);
+        x_0.setIndex(2, 0, 1);
+        x_0.setIndex(3, 0, 1);
+
+
+        r1.setIndex(0, 0, 0);
+        r1.setIndex(0, 1, 0);
+        r1.setIndex(0, 2, 0);
+        r1.setIndex(0, 3, 0);
+        r1.setIndex(0, 0, 0);
+        r1.setIndex(0, 1, 0);
+        r1.setIndex(0, 3, 0);
+        r1.setIndex(0, 0, 0);
+        r1.setIndex(0, 2, 0);
+        r1.setIndex(0, 1, 0);
+        r1.setIndex(0, 2, 0);
+        r1.setIndex(0, 3, 0);
+        r1.setIndex(0, 0, 0);
+        r1.setIndex(0, 1, 0);
+        r1.setIndex(0, 2, 0);
+        r1.setIndex(0, 3, 0);
+
+        r2.setIndex(0, 0, 0);
+        r2.setIndex(0, 1, 0);
+        r2.setIndex(0, 2, 0);
+        r2.setIndex(0, 3, 0);
+        r2.setIndex(0, 0, 4);
+        r2.setIndex(0, 1, 4);
+        r2.setIndex(0, 3, 4);
+        r2.setIndex(0, 0, 4);
+        r2.setIndex(0, 2, 8);
+        r2.setIndex(0, 1, 8);
+        r2.setIndex(0, 2, 8);
+        r2.setIndex(0, 3, 8);
+        r2.setIndex(0, 0, 12);
+        r2.setIndex(0, 1, 12);
+        r2.setIndex(0, 2, 12);
+        r2.setIndex(0, 3, 12);
+
+        r3.setIndex(0, 0, -12);
+        r3.setIndex(0, 1, -12);
+        r3.setIndex(0, 2, -12);
+        r3.setIndex(0, 3, -12);
+        r3.setIndex(0, 0, -8);
+        r3.setIndex(0, 1, -8);
+        r3.setIndex(0, 3, -8);
+        r3.setIndex(0, 0, -8);
+        r3.setIndex(0, 2, -4);
+        r3.setIndex(0, 1, -4);
+        r3.setIndex(0, 2, -4);
+        r3.setIndex(0, 3, -4);
+        r3.setIndex(0, 0, 0);
+        r3.setIndex(0, 1, 0);
+        r3.setIndex(0, 2, 0);
+        r3.setIndex(0, 3, 0);
+
+        x_1.setIndex(0, 0, 13);
+        x_1.setIndex(1, 0, 14);
+        x_1.setIndex(2, 0, 15);
+        x_1.setIndex(3, 0, 16);
+
+        x_2.setIndex(0, 0, 1);
+        x_2.setIndex(1, 0, 2);
+        x_2.setIndex(2, 0, 3);
+        x_2.setIndex(3, 0, 4);
+
+
     }
-    int bigArray[10][10]; //100 arrays de 10 elementos random
-    int reallyBig[1000];
-    int naturals[10];
+    Matrix f = Matrix(4,4);
+    Matrix g = Matrix(4,4);
+    Matrix r1 = Matrix(4,4);
+    Matrix r2 = Matrix(4,4);
+    Matrix r3 = Matrix(4,4);
+    Matrix x_0 = Matrix(4,1);
+    Matrix x_1 = Matrix(4,1);
+    Matrix x_2 = Matrix(4,1);
 };
 
-TEST_F (kNNTest, nombreTest){
 
-
-    int i = 0;
-    while (i < 10) {
-        naturals [i] = i;
-        i++;
-    }
-
-
+TEST_F (runTest, distance){
+    ASSERT_EQ( distance(g, x_1), r3);//Negative
+    ASSERT_EQ( distance(g, x_2), r2);//Positive
+    ASSERT_EQ( distance(f, x_0), r1); //Null matrix
 
 }
