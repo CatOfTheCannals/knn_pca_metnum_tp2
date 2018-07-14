1d99e24a8bcd1794bb039f3ed621c53bb0a70b67
CatOfTheCannals <carlosr.giudice@gmail.com>
2018-07-14 14:51:58 -0300

    now it works :D

--- data/pca_knn_cuantitative.cpp
+++ data/pca_knn_cuantitative.cpp
@@ -14,14 +14,14 @@ void pca_knn_cuantitative() {
     d.splitTrainFromTest(0); //when we pass zero, it sets test to one row
 
     int rows = d.getTrainImages().rows();
-    for(int alpha = 1; alpha < rows; alpha += rows / 10) {
+    for (int alpha = 1; alpha < rows; alpha += rows / 10) {
 
         std::cout << std::endl << "alpha: " << alpha << std::endl;
 
         d.trainPca(alpha, epsilon);
-        
-        for(int repetitions = 0; repetitions < 200; repetitions++){
-            for(int k = 1; k < rows; k++){
+
+        for (int repetitions = 0; repetitions < 200; repetitions++) {
+            for (int k = 1; k < rows; k++) {
                 std::cout << std::endl << "k: " << k << std::endl;
                 auto begin = GET_TIME;
                 d.pca_kNN_predict(k, epsilon);
@@ -30,6 +30,7 @@ void pca_knn_cuantitative() {
 
                 file << alpha << "," << k << "," << predict_time << std::endl;
                 std::cout << "time: " << predict_time << std::endl;
+            }
         }
     }
 }
