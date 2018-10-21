8bcaa0c3709fa214c91485e07c7d815ce3dececb
CatOfTheCannals <carlosr.giudice@gmail.com>
2018-10-21 12:54:25 -0300

    pca time exp stuff

--- data/pca_cuantitative.cpp
+++ data/pca_cuantitative.cpp
@@ -4,25 +4,25 @@ void pca_cuantitative() {
     double epsilon = 0.0001;
 
     std::ostringstream filename;
-    filename << "../../data/results/pca_cuantitative_reduced.csv";
+    filename << "../../data/results/pca_cuantitative_imdb.csv";
     ofstream file;
     file.open(filename.str());
-    file << "rows" << "," << "cols" << "," << "alpha" << "," << "time" << std::endl;
+    file << "rows"  << "," << "alpha" << "," << "time" << std::endl;
 
-    Dataset reduced = Dataset("../../test/casos_test/", "testRed.in");
-    // BIG BUG
-    Matrix images = reduced.getTrainImages().subMatrix(0, images.rows() - 1, 0, 10);
+    // Dataset d = Dataset::loadImdbVectorizedReviews();
+    // Matrix features = d.getTrainImages();
+    Matrix features = Matrix::random(50);
 
-    for(int rows = 2; rows < images.rows(); rows += images.rows() / 10){
-        for(int alpha = 1; alpha < images.cols(); alpha += images.cols() / 10){
+    for(int rows = 2; rows < features.rows(); rows += features.rows() / 10){
+        for(int alpha = 1; alpha < features.cols(); alpha += features.cols() / 10){
 
-            Matrix images_sub_sample = images.subMatrix(0, rows, 0, images.cols() - 1);
+            Matrix features_sub_sample = features.subMatrix(0, rows, 0, features.cols() - 1);
 
             auto begin = GET_TIME;
-            auto vectors_and_lambdas = pca(images_sub_sample, alpha, epsilon);
+            auto vectors_and_lambdas = pca(features_sub_sample , alpha, epsilon);
             auto end = GET_TIME;
 
-            file << images_sub_sample.rows() << "," << alpha
+            file << features_sub_sample.rows() << "," << alpha
                  << "," << GET_TIME_DELTA(begin, end) << std::endl;
         }
     }
