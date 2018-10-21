7cbe6c57243b0a87a08c026c02ce57ed83ee8099
Luciano Strika <lucianostrika44@gmail.com>
2018-10-21 14:16:55 -0300

    experimentacion cuali y cuantitativa para pca knn

--- data/correrExp.cpp
+++ data/correrExp.cpp
@@ -18,15 +18,18 @@
 
 int main(int argc, char** argv){
 
-    std::cout << std::endl << "Starting exp." << std::endl;
+    std::cout << std::endl << "Starting quali exp." << std::endl;
 
     //power_method_cuantitative(10, 4 , 25, 5);
     //pca_knn_cuantitative();
 
-    pca_cuantitative();
+    pca_knn_qualitative();
 
-    std::cout << std::endl << "Finished exp." << std::endl;
-/*
+    std::cout << std::endl << "Finished quali exp." << std::endl;
+    std::cout << std::endl << "Starting cuanti exp." << std::endl;
+    pca_knn_cuantitative();
+    std::cout << std::endl << "Done cuanti exp." << std::endl;
+  /*
     Matrix test_m = Matrix(4,4);
     for(int i = 0; i < 4; i++){
         test_m.setIndex(i,i, (double)(1.0 * (i+1)) );
