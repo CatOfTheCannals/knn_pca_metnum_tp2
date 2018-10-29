f318ff103f004c88b2058b11455498a3c2c1e1ee
CatOfTheCannals <carlosr.giudice@gmail.com>
2018-10-29 02:05:37 -0300

    small config changes to run several experimentations on parallel

--- data/correrExp.cpp
+++ data/correrExp.cpp
@@ -21,11 +21,13 @@ int main(int argc, char** argv){
     std::cout << std::endl << "Starting cuanti cuali exp." << std::endl;
 
     auto alphas = vector<int>({5,10,15,20,50,100, 500});
-    auto chunkPercentages = vector<double>({0.02, 0.04, 0.08, 0.2 , 0.4});
     auto neighbourhoodPercentualSizes = vector<double>({0.002, 0.004, 0.006, 0.01, 0.02, 0.05, 0.1, 0.15});
 
+    auto chunkPercentages = vector<double>({0.02, 0.08,  0.4});
+    // auto chunkPercentages = vector<double>({0.04, 0.2 });
+
     knn_qualitative_and_quantitative(chunkPercentages, neighbourhoodPercentualSizes);
-    pca_knn_qualitative_and_quantitative(alphas, chunkPercentages, neighbourhoodPercentualSizes);
+    // pca_knn_qualitative_and_quantitative(alphas, chunkPercentages, neighbourhoodPercentualSizes);
     std::cout << std::endl << "Done cuanti cuali exp." << std::endl;
 
     return 0;
