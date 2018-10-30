72eb20999e3002d2aecc5bab2ae703cd639e42eb
root <root@snapshots-migrator.com>
2018-10-30 00:00:19 +0000

    version falopa con muchos datosss skereee

--- data/correrExp.cpp
+++ data/correrExp.cpp
@@ -20,12 +20,11 @@ int main(int argc, char** argv){
 
     std::cout << std::endl << "Starting cuanti cuali exp." << std::endl;
 
-    auto alphas = vector<int>({5,10,15,20,50,100, 500});
-    //auto neighbourhoodPercentualSizes = vector<double>({0.002, 0.004, 0.006, 0.01, 0.02, 0.05, 0.1, 0.15});
-    auto neighbourhoodPercentualSizes = vector<double>({0.1, 0.15});
+    auto alphas = vector<int>({5,10,15,20,50,100,500,1200,1500});
+    auto neighbourhoodPercentualSizes = vector<double>({0.002, 0.004, 0.006, 0.01, 0.02, 0.05, 0.1, 0.15});
 
-    auto chunkPercentages = vector<double>({0.02, 0.08,  0.4});
-    // auto chunkPercentages = vector<double>({0.04, 0.2 });
+   // auto chunkPercentages = vector<double>({0.02, 0.08,  0.4});
+     auto chunkPercentages = vector<double>({0.04, 0.2, 0.5, 0.75});
 
     // knn_qualitative_and_quantitative(chunkPercentages, neighbourhoodPercentualSizes);
     pca_knn_qualitative_and_quantitative(alphas, chunkPercentages, neighbourhoodPercentualSizes);
